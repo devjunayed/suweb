183da0a1cc3a555aa08dd7c44938446829c1d48d
devjunayed <90998396+devjunayed@users.noreply.github.com>
2021-12-07 19:56:48 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Scripts for bottom to up button
+mybutton = document.getElementById("btu");
+mybutton.style.display ="none";
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop>30 || document.documentElement.scrollTop >30){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none";
+    }
+}
